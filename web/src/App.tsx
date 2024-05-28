d3ecc39c78952a1410c077c08b94249d866b013b
Grisha <you@example.com>
2024-05-28 22:56:29 +0300

    [u] add functional

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,20 @@
-function App() {
+import React, { useState } from 'react';
+import './styles/index.css';
+import planetImage from './assets/planet/green.svg'; // Убедитесь, что путь к изображению правильный
+
+const App: React.FC = () => {
+  const [count, setCount] = useState(0);
+
   return (
-    <div>
-      Hello World!
+    <div id="app">
+      <div className="counter">{count}</div>
+      <div
+        className="planet"
+        onClick={() => setCount(count + 1)}
+        style={{ backgroundImage: `url(${planetImage})` }}
+      ></div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
