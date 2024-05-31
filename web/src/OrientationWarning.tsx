cc5d5b22bcaca079feec82bbbb308cf5ffa5efec
Grisha <you@example.com>
2024-05-31 16:37:57 +0300

    [+] add OrientationWarning.tsx

---
+++ web/src/OrientationWarning.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrientationWarning: React.FC = () => {
+  return (
+    <div className="orientation-warning">
+      <div>Пожалуйста, поверните устройство в вертикальное положение.</div>
+    </div>
+  );
+};
+
+export default OrientationWarning;
