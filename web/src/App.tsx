ca56ff28835740acbcd8558b4046f89257ff1534
Grisha <you@example.com>
2024-05-31 16:38:33 +0300

    [u] update App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import './styles/index.css';
 import planetImage from './assets/planet/green.svg'; // Убедитесь, что путь к изображению правильный
+import { useOrientation } from './useOrientation';
+import OrientationWarning from './OrientationWarning';
 
 const App: React.FC = () => {
   const [count, setCount] = useState(0);
+  const isPortrait = useOrientation();
 
   return (
     <div id="app">
+      {!isPortrait && <OrientationWarning />}
       <div className="counter">{count}</div>
       <div
         className="planet"
