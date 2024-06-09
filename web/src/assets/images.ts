aead7f8948ec64af09b13d9bafcb7958a4edd14c
Grisha <grisha765@tuta.io>
2024-06-09 15:17:06 +0300

    [+] add images path

---
+++ web/src/assets/images.ts
@@ -0,0 +1,39 @@
+// src/assets/images.ts
+import planetImageGreen from './planet/green.svg';
+import planetImageBlue from './planet/blue.svg';
+import menuButtonImage from './buttons/menu_button.svg';
+import openedMenuImage from './opened_menu.svg';
+import shopButtonImage from './buttons/shop_button.svg';
+import voiceOnImage from './buttons/voice_on.svg';
+import voiceOffImage from './buttons/voice_off.svg';
+import twitterButtonImage from './buttons/connct_x.svg';
+import walletButtonImage from './buttons/connect_wallet.svg';
+import giftButtonImage from './buttons/gift_button.svg';
+import clansButtonImage from './buttons/clans_button.svg';
+import autoButtonImage from './buttons/auto_button.svg';
+import tool1ButtonImage from './buttons/tool_1_button.svg';
+import tool2ButtonImage from './buttons/tool_2_button.svg';
+import tool3ButtonImage from './buttons/tool_3_button.svg';
+import levelExpImage from './level_exp_info.svg';
+import resourseRareImage from './resourse_rare.svg'
+
+export {
+  planetImageGreen,
+  planetImageBlue,
+  menuButtonImage,
+  openedMenuImage,
+  shopButtonImage,
+  voiceOnImage,
+  voiceOffImage,
+  twitterButtonImage,
+  walletButtonImage,
+  giftButtonImage,
+  clansButtonImage,
+  autoButtonImage,
+  tool1ButtonImage,
+  tool2ButtonImage,
+  tool3ButtonImage,
+  levelExpImage,
+  resourseRareImage,
+};
+
