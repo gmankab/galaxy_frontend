22dee4a3c3c2edc4a221c241455888fa186a7421
Grisha <grisha765@tuta.io>
2024-06-09 14:01:40 +0300

    [r] change the address for requests

--- web/src/api.ts
+++ web/src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://paul.freemyip.com:8000',
+  baseURL: 'https://paul.freemyip.com:443',
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
