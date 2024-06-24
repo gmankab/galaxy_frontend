138a59a7c801bced4db3903047466e389963b804
demi9ka <grazz1wmel@gmail.com>
2024-06-24 15:54:17 +0200

    [r] refactor member component

--- src/app/components/ClanPage/member/index.tsx
+++ src/app/components/ClanPage/member/index.tsx
@@ -1,5 +1,4 @@
 import css from './css.module.css';
-import logo_skeleton from '@/assets/logo_skeleton.svg';
 
 export interface MemberProps {
   name_id: string;
@@ -11,7 +10,7 @@ export function Member({ count, name_id, logo }: MemberProps) {
   return (
     <div className={css.member}>
       <div className={css.lf_part}>
-        <img src={logo || logo_skeleton} className={css.user_logo} alt='' />
+        <div className={css.user_logo} ></div>
         <p className={css.name_id}>{name_id}</p>
       </div>
       <div className={css.rt_part}>
